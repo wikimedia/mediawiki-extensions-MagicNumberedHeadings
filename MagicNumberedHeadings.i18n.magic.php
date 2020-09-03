90536a24cf00f70ecef374537013524fa4762ae8
Christian Aistleitner <christian@quelltextlich.at>
2020-09-03 09:23:42 +0200

    Revert "Archive the extension"
    This reverts commit d1266f9f55d799350ee0d55936cb28fa6d14e14e.
    
    See https://www.mediawiki.org/w/index.php?title=Gerrit/New_repositories/Requests/Entries&diff=4069571&oldid=4068952
    
    Change-Id: I1e04a7da2aa66b9979aa809a6c498f7bc71f8dad

---
+++ MagicNumberedHeadings.i18n.magic.php
@@ -0,0 +1,15 @@
+<?php
+
+$magicWords = [];
+
+$magicWords['en'] = [
+	'MAG_NUMBEREDHEADINGS' => [ 0, '__NUMBEREDHEADINGS__' ],
+];
+
+$magicWords['de'] = [
+	'MAG_NUMBEREDHEADINGS' => [ 0, '__ÜBERSCHRIFTENNUMMERIERUNG__' ],
+];
+
+$magicWords['ksh'] = [
+	'MAG_NUMBEREDHEADINGS' => [ 0, '__ÖVVERSCHRIFTENUMMERIERE__' ],
+];
